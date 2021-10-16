b9510a1e31c0476858bd2005f34a3e0d45ba4ae6
ForeytKL22 <krforeyt@gmail.com>
2021-10-16 08:14:44 -0600

    got single note to populate

---
+++ Develop/db/store.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+// fs.readFile
+// fs.writeFile
+
+// how we are storing the state of our database
+
+// create a class called store
+
+// add method called 'read'
+
+// returns the json file being read
+
+// use readfile
+
+// write is going to take the notes in as a parameter
+// that is going to write the note to the json
+
+// next method: get notes
+
+// return what the notes are 
+
+// another method: add note
+// this will take in the notes as a parameter 
+// find a way to get the current notes and add the new note to it
+
+// remove note
+// this takes in a parameter of the note id - uses filter function
+// filter out notes and creates a new array
+
+
+// module.exports = new Store();
---
+++ Develop/routes/apiroutes.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const router = require('express').Router();
+const notes = require('../db/db.json');
+// const store = require('../db/store.js');
+const path = require('path');
+
+
+router.get('/notes', (req, res) => {
+    const results = notes;
+    res.json(results);
+    console.log(results);
+});
+
+router.post('/notes', (req, res) => {
+    console.log(JSON.stringify(req.body));
+    const results = notes;
+    const newNote = { title: req.body.title, text: req.body.text, id: 1 };
+
+    results.push(newNote);
+    console.log(results);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(results, null, 2));
+    });
+
+
+module.exports = router;
